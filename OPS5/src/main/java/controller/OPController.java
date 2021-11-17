306ee8c4818218f60159c9c47a3e77562a03e8d0
cassandra <cassandra.krause@student.uni-luebeck.de>
2021-11-17 11:12:10 +0100

    add content for optype

--- OPS5/src/main/java/controller/OPController.java
+++ OPS5/src/main/java/controller/OPController.java
@@ -25,6 +25,24 @@ public class OPController{
 	
     @FXML
 	public void initialize() {
+        /**
+        Result<Record1<Integer>> opcase = Main.dslContext.select(Tables.FALL.FALL_ID.as("op_case"))
+                .from(Tables.FALL)
+                .join(Tables.PATIENT)
+                .on(Tables.FALL.PAT_ID.eq(Tables.PATIENT.PAT_ID))
+                .where(Tables.PATIENT.PAT_ID.eq(2))
+                .fetch();
+
+        List<Integer> opcase_list = opcase.map(record -> record.getValue("op_case").);
+        opType.getItems().setAll(opcase_list);
+*/
+        Result<Record1<String>> typeresult = Main.dslContext.select(Tables.OP_TYP_ST.BESCHREIBUNG.as("opbeschreibung"))
+                .from(Tables.OP_TYP_ST)
+                .orderBy(Tables.OP_TYP_ST.OP_TYP.asc())
+                .fetch();
+        List<String> optyp_list = typeresult.map(record -> record.getValue("opbeschreibung").toString());
+        opType.getItems().setAll(optyp_list);
+
         Result<Record1<String>> result = Main.dslContext.select(Tables.OP_SAAL_ST.BESCHREIBUNG.as("opsaal"))
                 .from(Tables.OP_SAAL_ST)
                 .orderBy(Tables.OP_SAAL_ST.BESCHREIBUNG.asc())
