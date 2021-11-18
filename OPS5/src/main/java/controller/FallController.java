92a9a8553d494f6ceb25531c43baa6e5ac261315
cassandra <cassandra.krause@student.uni-luebeck.de>
2021-11-18 15:09:03 +0100

    add text fpr fall and patient pane

--- OPS5/src/main/java/controller/FallController.java
+++ OPS5/src/main/java/controller/FallController.java
@@ -1,7 +1,38 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 public class FallController {
+
+    @FXML
+    private TextField patient;
+
+    @FXML
+    private TextField falltyp;
+
+    @FXML
+    private TextField station;
+
+    @FXML
+    private DatePicker aufnahmedatum;
+
+    @FXML
+    private DatePicker entlassungsdatum;
+
+    @FXML
+    private Button speicherbutton;
+
+
+    @FXML
+    public void initialize() {
+        System.out.println("Initialize Fall-Tab!");
+    }
+
+    public void createFall(ActionEvent actionEvent) {
+        System.out.println("Creating Fall!");
+    }
 }
