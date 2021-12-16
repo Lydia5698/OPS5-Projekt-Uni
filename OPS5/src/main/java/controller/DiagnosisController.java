36b1b58a7b03d4aeb5e79ae36ac5631a5cb11a56
Lydia Günther <lydia.guenther@student.uni-luebeck.de>
2021-12-16 17:17:33 +0100

    Pub Med web view

--- OPS5/src/main/java/controller/DiagnosisController.java
+++ OPS5/src/main/java/controller/DiagnosisController.java
@@ -6,7 +6,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -18,6 +21,7 @@ import org.jooq.tools.json.JSONArray;
 import org.jooq.tools.json.JSONObject;
 import org.jooq.tools.json.JSONParser;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -377,5 +381,22 @@ public class DiagnosisController {
 		return true;
 
 	}
+
+	@FXML
+	public void openWebView() {
+		System.out.println("New Patient Window!");
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			fxmlLoader.setLocation(getClass().getResource("/fxml/WebView.fxml"));
+			Parent root = fxmlLoader.load();
+			Stage stage = new Stage();
+			stage.setScene(new Scene(root));
+			stage.show();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 
 }
