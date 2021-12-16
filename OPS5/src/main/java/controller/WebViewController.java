fdb5e6b815104c6af4f3ccf06b0b50ac42c82d9a
Lydia Günther <lydia.guenther@student.uni-luebeck.de>
2021-12-16 16:46:22 +0100

    webview import

---
+++ OPS5/src/main/java/controller/WebViewController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javafx.fxml.FXML;
+//import javafx.scene.web.WebView;
+import javafx.scene.web.WebView;
+
+public class WebViewController {
+
+    @FXML
+    private WebView pubMedView;
+
+}
