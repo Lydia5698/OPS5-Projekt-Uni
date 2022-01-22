851579d1fc2257ce71126b0590fd002c90d2baa7
Cassandra Krause <cassandra.krause@student.uni-luebeck.de>
2022-01-22 12:51:48 +0100

    remove some alerts from communication and fix a bug at insertion of diagnosis

--- OPS5/src/main/java/connection/MessageParser.java
+++ OPS5/src/main/java/connection/MessageParser.java
@@ -91,19 +91,6 @@ public class MessageParser {
                 return null;
             }
             else{
-                PV1 pv1 = adtMsg.getPV1();
-
-                Operation operation = new Operation();
-                operation.setErsteller("00000000");
-                operation.setStorniert(false);
-                operation.setErstellZeit(LocalDateTime.now());
-                operation.setFallId(Integer.parseInt(pv1.getSetIDPV1().getValue()));
-                operation.setBauchtuecherPrae(0);
-                operation.setBauchtuecherPost(0);
-
-                new OperationDao(Main.configuration).insert(operation);
-
-
                 List<DG1> dg1List = adtMsg.getDG1All();
                 for (DG1 dg1 : dg1List) {
                     Diagnose diagnose = new Diagnose();
@@ -115,8 +102,8 @@ public class MessageParser {
                     diagnose.setKlartextDiagnose(dg1.getDiagnosisDescription().getValue());
                     diagnose.setStorniert(false);
                     diagnoses.add(diagnose);
-                    return diagnoses;
                 }
+                return diagnoses;
              }
         } catch(HL7Exception e){
                 Platform.runLater(()->{
@@ -126,7 +113,6 @@ public class MessageParser {
                 });
                 return null;
         }
-        return null;
     }
 
 
@@ -135,14 +121,13 @@ public class MessageParser {
      * @param message the sent message
      * @return ture, if the message contains a diagnosis
      */
-    public static boolean a01WithDignosis(Message message){
+    public static boolean a01WithDiagnosis(Message message){
         ADT_A01 adt_a01 = (ADT_A01) message;
         try{
             return adt_a01.getDG1All().size() > 0;
         } catch(HL7Exception e){
             return false;
         }
-
     }
 
     /**
--- OPS5/src/main/java/connection/Server.java
+++ OPS5/src/main/java/connection/Server.java
@@ -5,16 +5,15 @@ import ca.uhn.hl7v2.app.*;
 import ca.uhn.hl7v2.model.Message;
 import ca.uhn.hl7v2.protocol.ReceivingApplication;
 import controller.CommunicationsController;
-import controller.MainController;
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import jooq.tables.daos.DiagnoseDao;
-import jooq.tables.pojos.Diagnose;
-import jooq.tables.pojos.Fall;
-import jooq.tables.pojos.Patient;
+import jooq.tables.daos.OperationDao;
+import jooq.tables.pojos.*;
 import main.Main;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -43,7 +42,7 @@ public class Server {
                         //dem Nutzer zeigen, dass das Kis einen neuen Patienten gesendet hat
                         Alert alert = new Alert(Alert.AlertType.INFORMATION);
                         alert.setTitle("Es wurde etwas geschickt");
-                        alert.setHeaderText("Das KIS hat einen neuen Patienten geschickt");
+                        alert.setHeaderText("Das KIS hat eine Nachricht gesendet");
                         alert.setContentText(encodedMessage);
                         alert.show();
                     });
@@ -58,32 +57,45 @@ public class Server {
                 //only insert the patient if it is a new patient
                 if(CommunicationsController.getInstance().isNewPatient(patient)){
                     CommunicationsController.insertNewPatient(patient);
-                }
+                    System.out.println("Neuer Patient eingefügt");
+                } //valider Patient
                 if(CommunicationsController.getInstance().canInsertPatient(patient)){
                     Fall fall = MessageParser.parseA01Case(message);
+                    //wenn es ein valider Fall ist
                     if(CommunicationsController.getInstance().canInsertCase(fall)){
+                        //wenn es ein neuer Fall ist wird er eingefügt
                         if(CommunicationsController.getInstance().isNewCase(fall)){
                             CommunicationsController.insertNewCase(fall);
-                            System.out.println("Patient und Fall eingefügt");
-                            Platform.runLater(()-> {
-                                Alert confirm = new Alert(Alert.AlertType.INFORMATION);
-                                confirm.setContentText("Die Datenbank wurde synchronisiert.");
-                                confirm.showAndWait();
-                            });
+                            System.out.println("Neuer Fall eingefügt");
                         }
-                        if(MessageParser.a01WithDignosis(message)){
+                        if(MessageParser.a01WithDiagnosis(message)){
                             List<Diagnose> diagnoseList = MessageParser.parseA01Diagnose(message);
                             assert diagnoseList != null;
                             for (Diagnose diagnose : diagnoseList) {
+                                //falls neue Diagnose muss diese eingefügt werden
                                 if(CommunicationsController.getInstance().isNewDiagnosis(diagnose)){
+                                    //falls noch keine Operation zu dem Fall exisitert, wird eine neue erstellt
+                                    if(new OperationDao(Main.configuration).fetchByFallId(fall.getFallId()).size() == 0){
+                                        Operation operation = new Operation();
+                                        operation.setErsteller("00000000");
+                                        operation.setStorniert(false);
+                                        operation.setErstellZeit(LocalDateTime.now());
+                                        operation.setFallId(fall.getFallId());
+                                        operation.setBauchtuecherPrae(0);
+                                        operation.setBauchtuecherPost(0);
+                                        new OperationDao(Main.configuration).insert(operation);
+                                        //setze die op id
+                                        diagnose.setOpId(new OperationDao(Main.configuration).fetchByFallId().get(0).getOpId());
+                                    }
+                                    else{
+                                        //falls es schon eine Operation zu dem Fall gibt, wird die diagnose der neusten Operation
+                                        //hinzugefügt
+                                        int newestOperation = new OperationDao(Main.configuration).fetchByFallId().size();
+                                        diagnose.setOpId(new OperationDao(Main.configuration).fetchByFallId().get(newestOperation).getOpId());
+                                    }
                                     new DiagnoseDao(Main.configuration).insert(diagnose);
                                 }
                             }
-                            Platform.runLater(()-> {
-                                Alert confirm = new Alert(Alert.AlertType.INFORMATION);
-                                confirm.setContentText("Die gesendeten Diagnosen wurden dem gesendeten Fall zugeordnet.");
-                                confirm.showAndWait();
-                            });
                         }
                     }
 
