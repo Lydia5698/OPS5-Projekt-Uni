6459a803e3cbc9778ebe4d05ceade1671eb19d20
Cassandra Krause <cassandra.krause@student.uni-luebeck.de>
2022-02-03 18:29:54 +0100

    fix casenumber in communication

--- OPS5/src/main/java/connection/MessageParser.java
+++ OPS5/src/main/java/connection/MessageParser.java
@@ -72,7 +72,7 @@ public class MessageParser {
         PV1 pv1 = adtMsg.getPV1();
 
         Fall fall = new Fall();
-        fall.setFallId(Integer.parseInt(pv1.getSetIDPV1().getValue()));
+        fall.setFallId(Integer.parseInt(pv1.getVisitNumber().getCx1_IDNumber().getValue()));
         fall.setPatId(Integer.parseInt(pid.getPatientID().getCx1_IDNumber().getValue()));
         if(pv1.getPatientClass().getValue() != null){fall.setFallTyp(pv1.getPatientClass().getValue().equals("I") ? 1 : 2);}
         fall.setAufnahmedatum(LocalDateTime.from(DateTimeFormatter.ofPattern("yyyyMMddHHmmss").parse(pv1.getAdmitDateTime().getTime().getValue())));
@@ -181,7 +181,6 @@ public class MessageParser {
 
             //pv1
             PV1 pv1 = bar05.getVISIT().getPV1();
-            pv1.getSetIDPV1().setValue(operation.getFallId().toString());
             if (fall.getFallTyp() != null) {
                 pv1.getPatientClass().setValue(Converter.fallTypConverter(fall.getFallTyp()).equals("stationär") ? "I" : "O");
             }
